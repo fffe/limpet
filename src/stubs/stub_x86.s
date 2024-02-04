beb1bc552548f518cbe66307f32749a13c80533b
fffe <fffe@users.noreply.github.com>
2024-02-04 09:11:39 +0000

    Force rel32 jmps on x86 and x64

--- src/stubs/stub_x86.s
+++ src/stubs/stub_x86.s
@@ -3,6 +3,10 @@
 
 payload_prefix_intel:
     call payload_prefix_intel_end
-    jmp payload_prefix_intel
+
+    # clang doesn't give a good way to force a rel32 jmp, so hardcode it
+    # this is basically 'jmp payload_prefix_intel'
+    .byte 0xe9
+    .long ((payload_prefix_intel) - 4) - .
 
 payload_prefix_intel_end:
